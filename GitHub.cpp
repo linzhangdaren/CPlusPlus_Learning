fc3bded4259545b7445a50012fd0e5fde0064fe6
linzhangdaren <739088568@qq.com>
2024-07-15 21:21:02 +0800

    v0.3

--- GitHub.cpp
+++ GitHub.cpp
@@ -1,8 +1,8 @@
-// 添加文件到暂存区
-git.add .
-// 查看暂存区状态
-git.status
-// 提交暂存区到本地仓库，并添加提交信息
-git.commit -m "commit message"
-// 将本地仓库推送到远程仓库
-git.push
+// // 添加文件到暂存区
+// git add .
+// // 查看暂存区状态
+// git status
+// // 提交暂存区到本地仓库，并添加提交信息
+// git commit -m "commit message"
+// // 将本地仓库推送到远程仓库
+// git push
