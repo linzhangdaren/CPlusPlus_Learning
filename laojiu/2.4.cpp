54f0ff2c52db012aabde4a7f00fa1698185f3344
linzhangdaren <739088568@qq.com>
2024-07-28 12:03:35 +0800

    老九

--- laojiu/2.4.cpp
+++ laojiu/2.4.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 using namespace std;
-
+#include <iomanip>
 int main()
 {
-    cout << "Hello World!" << endl;
+    float a = 12345.123456789;
+    double b = 12345.123456789;
+    long double c = 12345.123456789;
 
+    cout << fixed;            // 设置输出为定点表示法.固定显示
+    cout << setprecision(15); // 设置输出精度为15位小数.精度
+
+    cout << a << endl
+         << b << endl;
+    cout << c << endl; // 输出精度更高
     system("pause");
     return 0;
 }
---
+++ laojiu/3.2 cin_cout_Title.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    // 更改标题为:"C++输入输出"
+    SetConsoleTitle("C++输入输出");
+
+    system("pause");
+    return 0;
+}
