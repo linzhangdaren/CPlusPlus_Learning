b7031c3da30abe1ca25c1484a77dc933bd0e9eae
linzhangdaren <739088568@qq.com>
2024-08-10 01:34:34 +0800

    头文件之跨文件函数调用

---
+++ huoxing07/04 header file function.cpp
@@ -0,0 +1,6 @@
+int Max(int A, int B);
+
+int Max(int A, int B)
+{
+    return A > B ? A : B;
+}
---
+++ huoxing07/04 header file.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+// 跨文件调用函数
+
+int main()
+{
+
+    int Num1 = 1;
+    int Num2 = 2;
+    int Max(int, int); // 声明函数 可以不写形参名字  对应函数文名04 header file function.cpp
+    cout << Max(Num1, Num2) << endl;
+    return 0;
+}
