1bd9b22ede470e6dc3eec5c51110364c5b3181af
linzhangdaren <739088568@qq.com>
2024-08-25 21:05:38 +0800

    面向过程的static静态关键字

--- huoxing07/20 static fuction.cpp
+++ huoxing07/20 static fuction.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
 using namespace std;
-// 静态全局常量
+// 静态函数：函数前加static，函数只在本文件中有效，其他文件无法调用
+// 只能操作全局类型数据全局变量;静态全局函数;全局函数;静态全局变量
+// 不能操作局部类型数据不能操作级别低于自己的内容
 
-// static const int a = 10; //必须赋值 使用这一种 以下都对
-// const static int a = 10; 
-// static int const a = 10;
-
-// 静态局部变量：函数内部定义的静态变量
-
-void Say()
+static void Say()
 {
     static int TM = 0; // 程序执行时，只初始化一次，函数再次被调用时，不会重新初始化
     TM++;              // 每次调用函数时，都会对TM进行累加如果没有static 每次调用此函数TM都会被初始化为0
