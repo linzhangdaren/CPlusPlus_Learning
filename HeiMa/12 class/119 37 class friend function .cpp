c75159d6c7cd83325dd18e16c4507d95f8dbe1ca
linzhangdaren <739088568@qq.com>
2024-09-07 01:45:43 +0800

    友元函数 类中的实现

--- HeiMa/12 class/119 37 class friend function .cpp
+++ HeiMa/12 class/119 37 class friend function .cpp
@@ -4,7 +4,25 @@ using namespace std;
 
 // 类做友元
 
-class Building;
+class Building
+{
+public:
+    // 友元函数
+    friend class GoodGay;
+    // 成员变量
+    string m_SittingRoom; //"客厅"
+
+    // 构造函数
+    Building()
+    {
+        m_SittingRoom = "客厅";
+        m_BedRoom = "卧室";
+    }
+
+private:
+    // 成员变量
+    string m_BedRoom; // 卧室
+};
 
 class GoodGay
 {
@@ -17,10 +35,11 @@ public:
     {
         building = new Building;
     }
-    // 成员函数
+    // 成员升级为友元函数
     void visit()
     {
         cout << "好基友正在访问" << building->m_SittingRoom << endl;
+        cout << "好基友正在访问" << building->m_BedRoom << endl;
     }
     // 析构函数
     ~GoodGay()
@@ -31,23 +50,6 @@ public:
             building = nullptr;
         }
     }
-};
-
-class Building
-{
-public:
-    // 成员变量
-    string m_SittingRoom; //"客厅"
-    string m_BedRoom;     // 卧室
-    // 构造函数
-    Building()
-    {
-        m_SittingRoom = "客厅";
-        m_BedRoom = "卧室";
-    }
-
-    // private:
-    //     // 成员变量
 };
 
 void test01()
