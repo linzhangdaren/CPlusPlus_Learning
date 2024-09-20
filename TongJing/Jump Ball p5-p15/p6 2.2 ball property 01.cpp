79e7a80f20009596492ce68204dc370452ebcdab
linzhangdaren <739088568@qq.com>
2024-09-21 00:59:48 +0800

    字体配置

--- TongJing/Jump Ball p5-p15/p6 2.2 ball property 01.cpp
+++ TongJing/Jump Ball p5-p15/p6 2.2 ball property 01.cpp
@@ -1,29 +1,29 @@
-//#include <iostream>
-//#include <conio.h>
-//#include <graphics.h>
-//using namespace std;
-////边长函数
-//void Side(int width,int height) {
-//	int side;
-//	side=(width+height)*2;
-//	cout<<"边长为"<<side<<endl;
-//}
-////面积函数
-//void Area(int width,int height) {
-//	int area;
-//	area=width*height;
-//	cout << "面积为" << area << endl;
-//}
-//
-//
-//int main() {
-////长方形宽20高10,求边长和面积
-//	int width, height;
-//	width=20;
-//	height=10;
-//	Side(width,height);
-//	Area(width,height);
-//
-//
-//	return 0;
-//}
+// #include <iostream>
+// #include <conio.h>
+// #include <graphics.h>
+// using namespace std;
+// //边长函数
+// void Side(int width,int height) {
+// 	int side;
+// 	side=(width+height)*2;
+// 	cout<<"边长为"<<side<<endl;
+// }
+// //面积函数
+// void Area(int width,int height) {
+// 	int area;
+// 	area=width*height;
+// 	cout << "面积为" << area << endl;
+// }
+
+
+// int main() {
+// //长方形宽20高10,求边长和面积
+// 	int width, height;
+// 	width=20;
+// 	height=10;
+// 	Side(width,height);
+// 	Area(width,height);
+
+
+// 	return 0;
+// }
