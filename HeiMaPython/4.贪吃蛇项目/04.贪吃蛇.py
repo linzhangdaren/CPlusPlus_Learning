f9f72ec7c8437260a288af8cb1d752845265c1a7
linzhangdaren <739088568@qq.com>
2025-05-23 16:34:35 +0800

    贪吃蛇画线

--- HeiMaPython/4.贪吃蛇项目/04.贪吃蛇.py
+++ HeiMaPython/4.贪吃蛇项目/04.贪吃蛇.py
@@ -1,3 +1,14 @@
+'''
+1准备pygame开发环境窗口标题
+    a.准备背景图
+    b.准备蛇头
+2.while循环
+    a.处理用户输入事件
+    b.处理游戏逻辑
+    c.渲染界面
+    d.设置帧率
+'''
+
 #导入pygame库
 import pygame
 #初始化pygame
@@ -12,18 +23,19 @@ icon=pygame.image.load("plane.png")
 pygame.display.set_icon(icon)
 #设置帧率
 clock=pygame.time.Clock()
+#设置背景颜色
+color_bg=(128,128,128)
+#设置线颜色
+color_line=(255,255,255)
 #
+
 
 #循环窗口固定住
 while True:
-    #获取事件
-    for event in pygame.event.get():
-        #判断事件是否为退出事件
-        if event.type==pygame.QUIT:
-            #退出游戏
-            pygame.quit()
-            # exit()
-            
+    #a.处理用户输入事件
+    for event in pygame.event.get():#判断事件是否为退出事件        
+        if event.type==pygame.QUIT:#退出游戏            
+            pygame.quit()# exit()
         #判断事件是否为按键按下事件
         elif event.type==pygame.KEYDOWN:
             print("按键按下",event.key)#测试按键            
@@ -36,16 +48,18 @@ while True:
             elif event.key==pygame.K_RIGHT:
                 print("右")
             elif event.key==pygame.K_ESCAPE:
-                #退出游戏
-                pygame.quit()
+                pygame.quit()#退出游戏
                 # exit()
                 
-
-    #填充背景色
-    screen.fill((128,128,128))#RGB
-    #设置帧率
-    clock.tick(60)
-    #显示帧率
-    # print(clock.get_fps())
-    #刷新界面
-    pygame.display.flip()
+    # b.处理游戏逻辑
+    
+    # c.渲染界面
+    screen.fill(color_bg)#RGB填充颜色
+    #画线
+    pygame.draw.line(screen,color_line,(0,0),(300,100),1)
+    
+    pygame.display.flip()#刷新窗口
+    
+    # d.设置帧率
+    clock.tick(60)    
+    # print(clock.get_fps())#显示帧率
