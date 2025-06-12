96cc73fdecf39b075655b725b7f990a00c7f2e5b
linzhangdaren <739088568@qq.com>
2025-06-12 12:10:38 +0800

    线程传参的方式 元组&字典

--- Network/HeiMa/17.0.线程传参.py
+++ Network/HeiMa/17.0.线程传参.py
@@ -7,8 +7,19 @@ def singing(name,age=18,score=100):
         time.sleep(1)
 
 if __name__ == '__main__':
+    #1.元组传参
     t1=threading.Thread(target=singing,args=("张靓颖",19,99))
     t2=threading.Thread(target=singing,args=("王力宏",20,88))
+    
+    #2.字典传参
+    #如果用kwargs就相当域字典了 这样可以随意传参不用在意顺序:
+    # t1=threading.Thread(target=singing,kwargs={"name":"张靓颖","age":19,"score":99})
+    # t2=threading.Thread(target=singing,kwargs={"name":"王力宏","age":20,"score":88})
+    
+    #3.元组+字典传参(元组和字典传参不能重复你135我246)
+    # t1=threading.Thread(target=singing,args=("张靓颖",),kwargs={"age":19,"score":99})
+    # t2=threading.Thread(target=singing,args=("王力宏",),kwargs={"age":20,"score":88})
+    
     t1.start()
     t2.start()
     
