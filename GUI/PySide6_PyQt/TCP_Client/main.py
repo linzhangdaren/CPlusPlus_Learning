9d7f626fa74334236fd9d838e3619dac6542eeb5
linzhangdaren <739088568@qq.com>
2025-06-20 23:35:52 +0800

    主窗口正常调用其他窗口

--- GUI/PySide6_PyQt/TCP_Client/main.py
+++ GUI/PySide6_PyQt/TCP_Client/main.py
@@ -5,23 +5,28 @@ import sys
 
 from ui.main_window_ui import Ui_MainWindow#导入界面设计UI的名字
 
+from net_assist_widget import NetAssistWidget#导入单独设计的widget界面
+
 class MyWindow(QMainWindow):#继承QMainWindow类 这个名字在UI文件中class:
     def __init__(self):
         super().__init__()
 
         self.ui = Ui_MainWindow()#实例化界面设计类
         self.ui.setupUi(self)#调用setupUi方法
+        self.init_ui()#构造函数调用初始化界面
 
-        # self.init_ui()#构造函数调用初始化界面
-        
 #______________只需要常修改的地方_________________       
-    def on_btn_clicked(self):
-        print("按钮被点击了")
+    # def on_btn_clicked(self):
+    #     print("按钮被点击了")
         
     
     def init_ui(self):
-        self.ui.pushButton.clicked.connect(self.on_btn_clicked)#绑定按钮点击事件处理函数 btn_send是在界面设计中自定义按钮的名字
- #______________只需要常修改的地方_________________          
+        #绑定按钮点击事件处理函数 btn_send是在界面设计中自定义按钮的名字
+        # self.ui.pushButton.clicked.connect(self.on_btn_clicked)
+        Net=NetAssistWidget(self)#这个函数调用了NetAssistWidget()
+        self.ui.tabWidget.addTab(Net, "网络助手")
+    
+#______________只需要常修改的地方_________________          
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- GUI/PySide6_PyQt/TCP_Client/net_assist_widget.py
+++ GUI/PySide6_PyQt/TCP_Client/net_assist_widget.py
@@ -5,9 +5,9 @@ import sys
 
 from ui.net_assist_widget_ui import Ui_NetAssistWidget#导入界面设计UI的名字
 
-class MyWindow(QWidget):#继承QMainWindow类 这个名字在UI文件中class:
-    def __init__(self):
-        super().__init__()
+class NetAssistWidget(QWidget):#继承QMainWindow类 这个名字在UI文件中class:
+    def __init__(self,parent=None):
+        super().__init__(parent)
 
         self.ui = Ui_NetAssistWidget()#实例化界面设计类
         self.ui.setupUi(self)#调用setupUi方法
@@ -15,12 +15,13 @@ class MyWindow(QWidget):#继承QMainWindow类 这个名字在UI文件中class:
         # self.init_ui()#构造函数调用初始化界面
         
 #______________只需要常修改的地方_________________       
-    def on_btn_clicked(self):
-        print("按钮被点击了")
+    # def on_btn_clicked(self):
+        # print("按钮被点击了")
         
     
-    def init_ui(self):
-        self.ui.pushButton.clicked.connect(self.on_btn_clicked)#绑定按钮点击事件处理函数 btn_send是在界面设计中自定义按钮的名字
+    # def init_ui(self):
+        # self.ui.pushButton.clicked.connect(self.on_btn_clicked)
+        # #绑定按钮点击事件处理函数 btn_send是在界面设计中自定义按钮的名字
 #______________只需要常修改的地方_________________          
 
 if __name__ == '__main__':
@@ -28,7 +29,7 @@ if __name__ == '__main__':
     
     
     
-    window=MyWindow()
+    window=NetAssistWidget()
     window.show()
     
     sys.exit(app.exec())#未关闭之前持续刷新界面
