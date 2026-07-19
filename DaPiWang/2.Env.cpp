a1693d46e04df2df1a9125037c6100c2d6b7c22a
linzhangdaren <739088568@qq.com>
2026-07-19 20:46:33 +0800

    dapiwang

---
+++ DaPiWang/2.Env.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+int main()
+{
+    // 设置控制台输出代码页为UTF-8 (65001)
+    SetConsoleOutputCP(65001);
+    // 设置控制台输入代码页为UTF-8 (65001)
+    SetConsoleCP(65001);
+
+    cout << "你好!" << endl;
+    // 暂停窗口
+    system("pause");
+    return 0;
+}
